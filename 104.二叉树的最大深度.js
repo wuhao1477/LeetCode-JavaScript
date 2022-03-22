d301b3872d46d6ea80eb136001ce94fd5032acb0
wuhao1477 <1477681049@qq.com>
2022-03-22 20:29:49 +0800

    打卡2022.03.22

---
+++ 104.二叉树的最大深度.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+ var maxDepth = function (root) {
+    if (root == null) {
+      return 0;
+    }
+    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+  };
+  
+  let a = {
+    val: 3,
+    left: { val: 9, left: null, right: null },
+    right: {
+      val: 20,
+      left: { val: 15, left: null, right: null },
+      right: { val: 7, left: { val: 7, left: null, right: null }, right: null },
+    },
+  };
+  
+  console.log(`测试结果为：${maxDepth(a)}`);
+  
